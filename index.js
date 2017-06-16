1d4cb3d5b47f7b59b100b4a9d79fd2ac72ef748e
Christian Tang <git@tcdev.dk>
2017-06-16 09:25:03 +0200

    Snazzied

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 // standard ANSI Colours
-const black = '#323232';
-const red = '#C0392B';
-const green = '#27AE60';
-const yellow = '#F39C12';
-const blue = '#2980B9';
-const magenta = '#8E44AD';
-const cyan = '#2980B9';
-const white = '#ECF0F1';
-const lightBlack = '#95A5A6';
-const lightRed = '#E74C3C';
-const lightGreen = '#2ECC71';
-const lightYellow = '#F1C40F';
-const lightBlue = '#3498DB';
-const lightMagenta = '#9B59B6';
-const lightCyan = '#3498DB';
-const lightWhite = '#ffffff';
+const black = '#323232'
+const red = '#C0392B'
+const green = '#27AE60'
+const yellow = '#F39C12'
+const blue = '#2980B9'
+const magenta = '#8E44AD'
+const cyan = '#2980B9'
+const white = '#ECF0F1'
+const lightBlack = '#95A5A6'
+const lightRed = '#E74C3C'
+const lightGreen = '#2ECC71'
+const lightYellow = '#F1C40F'
+const lightBlue = '#3498DB'
+const lightMagenta = '#9B59B6'
+const lightCyan = '#3498DB'
+const lightWhite = '#ffffff'
 
 // custom colours
-const cobaltBackground = '#34495E';
-const variableBlue = '#0d3a58';
-const dustyBlue = '#34424C';
-const highlightBlue = '#1F4662';
+const cobaltBackground = '#34495E'
 
 exports.decorateConfig = (config) => Object.assign({}, config, {
   cursorColor: lightYellow,
@@ -29,8 +26,6 @@ exports.decorateConfig = (config) => Object.assign({}, config, {
   borderColor: 'rgba(255,255,255,0.05)',
   css: `
     ${config.css || ''}
-	* {
-	}
     .tab_tab:before {
       border-left: 1px solid;
     }
@@ -57,6 +52,6 @@ exports.decorateConfig = (config) => Object.assign({}, config, {
     lightBlue,
     lightMagenta,
     lightCyan,
-    lightWhite,
-  },
-});
+    lightWhite
+  }
+})
