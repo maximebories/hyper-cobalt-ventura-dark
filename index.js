adede778ef317d061b1e481006036c6bd52d011a
Maxime Bories <30550722+maximebories@users.noreply.github.com>
2022-11-30 21:58:08 +0100

    Ventura Sensitive Design

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+module.exports.onWindow = browserWindow =>
+  browserWindow.setVibrancy("ultra-dark");
+
 const colors = {
   black: '#323232',
   red: '#C0392B',
@@ -15,8 +18,10 @@ const colors = {
   lightMagenta: '#9B59B6',
   lightCyan: '#3498DB',
   lightWhite: '#FFFFFF'
-}
-const backgroundColor = '#34495E'
+};
+const backgroundColor = '#383838';
+const lightFrostyGreen = '#B9F6CA';
+const overlap = "rgba(0, 0, 0, .15)";
 
 exports.decorateConfig = (config) => Object.assign({}, config, {
   colors,
@@ -30,5 +35,74 @@ exports.decorateConfig = (config) => Object.assign({}, config, {
     x-screen x-row span {
       font-variant-ligatures: initial !important;
     }
-  `
-})
+  `,
+  css: `
+  ${config.css || ''}
+  .hyper_main {
+    border: none !important;
+  }
+  .header_header {
+    background-color: #262626 !important;
+    border-bottom: 1px solid #000 !important;
+  }
+  .tabs_borderShim {
+    border-color: transparent !important;
+  }
+  .tab_tab {
+    border: 0;
+  }
+
+  .tab_textActive {
+    background: rgba(255, 255, 255, .05);
+    color: ${lightFrostyGreen};
+  }
+  .tab_textActive i{
+    color: ${lightFrostyGreen};
+  }
+  .hyper-search-wrapper {
+      border: 0 !important;
+      padding: 0 !important;
+      background-color: transparent !important;
+      display: flex;
+      opacity: 0.8 !important;
+    }
+  .hyper-search-wrapper button {
+    top: 0 !important;
+    opacity: 0.8 !important;
+    padding: 0 6px;
+    cursor: pointer;
+  }
+  .hyper-search-wrapper button:hover {
+    opacity: 1.0 !important;
+  }
+  .hyper-search-wrapper button:nth-of-type(1) {
+    border-radius: 4px 0 0 4px !important;
+    border-right: 1px solid #ddd !important;
+  }
+  .hyper-search-wrapper button:nth-of-type(2) {
+    border-radius: 0 4px 4px 0 !important;
+  }
+  .hyper-search-wrapper:before {
+    width: 20px;
+    color: #000;
+    position: absolute;
+    content: "🔍";
+    font-size: 10px;
+    margin: 7px;
+    z-index: 999;
+  }
+  #hyper-search-input {
+    background-color: #fff !important;
+    border-radius: 4px;
+    box-shadow: 0 1px 10px rgba(0, 0, 0, 0.5);
+    padding: 3px 6px 3px 24px !important;
+    color: #000 !important;
+    opacity: 0.9 !important;
+    margin-right: 2px;
+  }
+  #hyper-search-input:focus {
+    opacity: 1.0 !important;
+    box-shadow: 0 1px 10px rgba(0, 0, 0, 1.0);
+  }
+`
+});
